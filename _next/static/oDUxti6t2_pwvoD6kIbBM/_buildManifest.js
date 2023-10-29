794ffd001b895e88416ecbf64ef8879aeb91cf89
liamzebedee <liamzebedee@yahoo.com.au>
2023-10-29 20:13:55 +1100

    generate/update site

---
+++ _next/static/oDUxti6t2_pwvoD6kIbBM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-c0d3dfbd7fecad42.js","static/chunks/pages/index-46e5cf75f80f31b4.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
